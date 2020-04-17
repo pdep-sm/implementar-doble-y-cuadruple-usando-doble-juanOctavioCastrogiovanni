4a88d5b8992a816a73b8cae285c2a90e1f76a3a5
juanOctavioCastrogiovanni <Jcastrogiovanni@est.frba.utn.edu.ar>
2020-04-16 23:12:20 -0300

    Agregando punto 1

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,40 @@
 module Library where
 import PdePreludat
+import Data.Char
 
-doble = (* 2)
-cuadruple = doble.doble
+type Desgaste = Number
+type Patente = String
+type Fecha = (Number, Number, Number)
+ 
+
+anio :: Fecha -> Number
+anio (_, _, year) = year
+
+
+
+data Auto = Auto {
+ patente :: Patente,
+ desgasteLlantas :: [Desgaste],
+ rpm :: Number,
+ temperaturaAgua :: Number,
+ ultimoArreglo :: Fecha
+} deriving Show
+
+
+
+    
+
+
+presupuestoReparacion patente
+    | (length patente == 7) = 12500
+    | ("dj0000" <= patente) && (patente <= "nb0000") || ("DJ0000" <= patente) && (patente <= "NB0000") = calculoPatental patente
+    | otherwise  = 15000 
+ 
+ultimoNumero :: String -> Bool
+ultimoNumero patente = last patente == '4'
+
+calculoPatental patente 
+   | ultimoNumero patente = 18000
+   | otherwise = 20000
+
+   
